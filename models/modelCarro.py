7cdfd9c2277bb3279a80f26846e796f7ee0e25ef
Natanael da Silva <dasilvanatanael700@gmail.com>
2020-05-24 00:09:30 -0300

    :sparkles: Trabalho Programação Net 2
    Trabalho de Programação de Internet2, API,Envio de E-mail.

--- models/modelCarro.py
+++ models/modelCarro.py
@@ -8,6 +8,8 @@ class Carro(db.Model):
     cor = db.Column(db.String(30), nullable=False)
     ano = db.Column(db.Integer, nullable=False)
     preco = db.Column(db.Float, nullable=False)
+    foto = db.Column(db.String(120), nullable=False)
+    destaque = db.Column(db.String(1), nullable=False)
     data_cad = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     marca_id = db.Column(db.Integer, db.ForeignKey(
@@ -22,6 +24,8 @@ class Carro(db.Model):
             'cor': self.cor,
             'ano': self.ano,
             'preco': self.preco,
+            'foto' : self.foto,
+            'destaque': self.destaque,
             'marca_id': self.marca_id,
             'marca': self.marca.nome
         }
@@ -33,5 +37,7 @@ class Carro(db.Model):
         cor = json_carros.get('cor')
         ano = json_carros.get('ano')
         preco = json_carros.get('preco')
+        foto = json_carros.get('foto')
+        destaque = json_carros.get('destaque')
         marca_id = json_carros.get('marca_id')
-        return Carro(modelo=modelo, cor=cor, ano=ano, preco=preco, marca_id=marca_id)
+        return Carro(modelo=modelo, cor=cor, ano=ano, preco=preco, foto=foto, destaque=destaque, marca_id=marca_id)
--- resources/carros.py
+++ resources/carros.py
@@ -13,9 +13,80 @@ def listagem():
 
 
 @carros.route('/carros', methods=['POST'])
-@jwt_required
+# @jwt_required
 def inclusao():
     carro = Carro.from_json(request.json)
     db.session.add(carro)
     db.session.commit()
     return jsonify(carro.to_json()), 201
+
+
+# Parte 1 do Trabalho
+
+@carros.errorhandler(404)
+def id_invalido(error):
+    return jsonify({'id': 0, 'message': 'not found'}), 404  
+
+
+@carros.route('/carros/<int:id>', methods=['PUT'])
+def alteracao(id):
+    # obtém o registro a ser alterado (ou gera um erro 404 - not found)
+    carro = Carro.query.get_or_404(id)
+    
+    # recupera os dados enviados na requisição
+    carro.modelo = request.json['modelo']
+    carro.cor = request.json['cor']
+    carro.ano = request.json['ano']
+    carro.preco = request.json['preco']
+    carro.foto = request.json['foto']
+    carro.destaque = request.json['destaque']
+    carro.marca_id = request.json['marca_id']
+    
+    # altera (pois o id já existe)    
+    db.session.add(carro)
+    db.session.commit()
+    return jsonify(carro.to_json()), 204
+
+
+@carros.route('/carros/<int:id>')
+def consulta(id):
+    # obtém o registro a ser alterado (ou gera um erro 404 - not found)
+    carro = Carro.query.get_or_404(id)
+    return jsonify(carro.to_json()), 200
+
+
+@carros.route('/carros/<int:id>', methods=['DELETE'])
+def exclui(id):
+    Carro.query.filter_by(id=id).delete()
+    db.session.commit()
+    return jsonify({'id': id, 'message': 'Carro excluído com sucesso'}), 200
+
+# Parte 2 do Trabalho
+@carros.route('/carros/destaque')
+def destaqueCarro():
+    carros = Carro.query.order_by(Carro.modelo).filter(Carro.destaque=='x').all()
+    return jsonify([carro.to_json() for carro in carros])
+ 
+ # Parte 3 do Trabalho 
+@carros.route('/carros/destacar/<int:id>',methods=['PUT'])
+def destacarCarro(id):
+    # obtém o registro a ser alterado (ou gera um erro 404 - not found)
+    carro = Carro.query.get_or_404(id)
+
+    # recupera os dados enviados na requisição
+    carro.destaque = request.json['destaque']
+
+    #   Altera pois ja existe
+    db.session.add(carro)
+    db.session.commit()
+    return jsonify(carro.to_json()), 204
+
+# Parte 4 do Trabalho
+@carros.route('/carros/filtro/<palavra>')
+def pesquisa(palavra):
+    # obtém todos os registros da tabela veiculos em ordem de modelo
+        carros = Carro.query.order_by(Carro.modelo).filter(Carro.modelo.like(f'%{palavra}%'))
+        if  carros:
+            return jsonify([carro.to_json() for carro in carros])  
+        else:
+            return jsonify({'Não foi encontrado veiculos com esse modelo'}), 404 
